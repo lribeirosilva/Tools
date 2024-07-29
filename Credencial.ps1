6a6e6eb5fecaae7f2843e714ac050fd08a418a32
Leandro Ribeiro <154350898+lribeirosilva@users.noreply.github.com>
2024-07-29 07:48:54 -0300

    Add files via upload

---
+++ Credencial.ps1
@@ -0,0 +1,9 @@
+﻿
+$Credential = Get-Credential
+
+$Credential.Username
+
+$ClearTextPassword = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($Credential.Password))
+
+Write-Host $ClearTextPassword
+
